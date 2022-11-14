8d439e97a265f33009c0cb03d8afadf620f1c174
a2pi <a2pi@users.noreply.github.com>
2022-11-14 19:31:38 +0100

    added some new pages (still in progrss)

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -77,6 +77,13 @@ const routes: RouteRecordRaw[] = [
       { path: '', component: () => import('pages/prom.vue') },
     ],
   },
+  {
+    path: '/patfile',
+    component: () => import('layouts/DigitalPromLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/patFile.vue') },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
