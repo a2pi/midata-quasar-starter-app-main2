a30d62168493740cdc5e5eb510053c1844b4132e
a2pi <a2pi@users.noreply.github.com>
2022-11-20 23:09:55 +0100

    Patient Search working (primitive search method).

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,20 +4,7 @@
     <form >
 
 <login-card></login-card>
-      <!--<img src="../assets/digitalprom/Logo.png"  class="logo">
- <div class="form-field">
-    <input type="email" placeholder="Username" required/>
-  </div>
 
-
-  <div class="form-field">
-    <input type="password" placeholder="Password" required/>                         </div>
-
-
-  <div class="form-field">
-    <<button class="btn" type="submit" onclick="$router.replace(’pages/prom.vue'’)">Log in</button>
-    <q-btn unelevated color="grey" label="Login" to="patfile" />
-  </div>-->
 </form>
   </q-page>
 </template>
